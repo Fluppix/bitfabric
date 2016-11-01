5aad337723ae755e7c8fb7963b16533dbea55821
Kristoffer Eklund <kristoffereklund1@gmail.com>
2016-11-01 23:27:36 +0100

    Fix edit guild returning blank page

--- Guild/Http/Controllers/Guild/EditController.php
+++ Guild/Http/Controllers/Guild/EditController.php
@@ -10,6 +10,7 @@ class EditController extends Controller
     /**
      * Show the edit guild form to the user.
      *
+     * @param  \Bitaac\Guild\Models\Guild  $guild
      * @return \Illuminate\Http\Response
      */
     public function form($guild)
@@ -20,29 +21,31 @@ class EditController extends Controller
     /**
      * Process the edition.
      *
+     * @param  \Bitaac\Guild\Http\Requests\Guild\EditRequest $request
+     * @param  \Bitaac\Guild\Models\Guild                    $guild
      * @return \Illuminate\Http\Response
      */
     public function post(EditRequest $request, $guild)
     {
         if ($request->has('description')) {
             $guild->bitaac->description = $request->get('description');
-            $guild->bitaac->save();
         }
 
         if ($request->hasFile('logo')) {
             $file = $request->file('logo');
-
             $guild->bitaac->logo = public_path("guild_avatars/{$guild->id}.gif");
-
             $file->move(public_path('guild_avatars'), "{$guild->id}.gif");
-
-            $guild->bitaac->save();
         }
+
+        $guild->bitaac->save();
+
+        return back()->withSuccess('Your guild has been edited.');
     }
 
     /**
      * Delete current logo.
      *
+     * @param  \Bitaac\Guild\Models\Guild  $guild
      * @return \Illuminate\Http\Response
      */
     public function deletelogo($guild)
